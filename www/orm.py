11df35ba96c0e3db8e63ed2e2e8e15d77d82e335
ReedSun <sunhongzhao@foxmail.com>
2016-11-01 09:29:38 +0800

    day3 语法错误修改

--- www/orm.py
+++ www/orm.py
@@ -97,12 +97,12 @@ class Field(object):
 
     # 定制输出信息为 类名，列的类型，列名
     def __str__(self):
-        return "<%s, %s:%s>" % (self.__class__.name, self.column_type, self.name)
+        return "<%s, %s:%s>" % (self.__class__.__name__, self.column_type, self.name)
 
 
 # 字符串域
 class StringField(Field):
-    def __init__(self, name=None, primary_key=False, default=None, ddl=varchar(100)):
+    def __init__(self, name=None, primary_key=False, default=None, ddl="varchar(100)"):
         super().__init__(name, ddl, primary_key, default)
 
 
@@ -111,15 +111,18 @@ class IntegerField(Field):
     def __init__(self, name=None, primary_key=False, default=0):
         super().__init__(name, "bigint", primary_key, default)
 
+
 # 布尔数域
 class BooleanField(Field):
     def __init__(self, name=None, primary_key=False, default=False):
         super().__init__(name, "boolean", primary_key, default)
 
+
 # 浮点数域
 class FloatField(Field):
     def __init__(self, name=None, primary_key=False, default=0.0):
         super().__init__(name, "real", primary_key, default)
+
 
 # 文本域
 class TextField(Field):
@@ -133,7 +136,7 @@ class TextField(Field):
 class ModelMetaclass(type):
     def __new__(cls, name, bases, attrs):
         # 排除Model类本身
-        if name == Model:
+        if name == "Model":
             return type.__new__(cls, name, bases, attrs)
         # 获取table名称
         tableName = attrs.get("__table__", None) or name
@@ -164,12 +167,11 @@ class ModelMetaclass(type):
         attrs["__mappings__"] = mappings  # 保存属性和列的映射关系
         attrs["__table"] = tableName  # 表名
         attrs["__primary_key__"] = primaryKey  # 主键属性名
-        attrs["__field__"] = fields  # 除主键外的属性名
+        attrs["__fields__"] = fields  # 除主键外的属性名
         # 构造默认的SELECT, INSERT, UPDATE, DELETE语句
         # 以下都是sql语句
         attrs["__select__"] = "select `%s`, %s from `%s`" % (primaryKey, ",".join(escaped_fields), tableName)
-        attrs["__insert__"] = " insert into `%s` (%s, `%s`) values (%s)" % (
-        tableName, ",".join(escaped_fields), primaryKey, create_args_string(len(escaped_fields() + 1)))
+        attrs["__insert__"] = " insert into `%s` (%s, `%s`) values (%s)" % (tableName, ",".join(escaped_fields), primaryKey, create_args_string(len(escaped_fields)+1))
         attrs["__update__"] = "update `%s` set %s where `%s`=?" % (
         tableName, ",".join(map(lambda f: "`%s`=?" % (mappings.get(f).name or f), fields)), primaryKey)
         attrs["__delete__"] = "delete from `%s` where `%s`=?" % (tableName, primaryKey)
@@ -233,7 +235,7 @@ class Model(dict, metaclass=ModelMetaclass):
 
     # findAll() - 根据WHERE条件查找
     @classmethod
-    async def findall(cls, where=None, args=None, **kw):
+    async def findAll(cls, where=None, args=None, **kw):
         # sql语句不太会。。这里好像是添加了几个参数 where、args、OrderBy、limit
         sql = [cls.__select__]
         # 如果有where参数就在sql语句中添加字符串where和参数where
@@ -295,7 +297,3 @@ class Model(dict, metaclass=ModelMetaclass):
         rows = await execute(self.__delete__, args)
         if rows != 1:
             logging.warning('failed to remove by primary key: affected rows: %s' % rows)
-
-
-
-
